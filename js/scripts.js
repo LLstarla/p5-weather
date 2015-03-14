92164efeaf78bebcd6a7ae91b09525668533a76c
LLstarla <loraleestarla@me.com>
2015-03-14 00:25:13 -0700

    v4 skytracker 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,127 @@
+// SKY TRACKER 
 
-$.simpleWeather({
-    
-    location: '99004', // change zip
-    unit: 'f',
-    
-    // Get _weather_ object
-    success: function(weather) {
-      
-      // Get & store temperature
-      var temp = weather.temp;
-      // Get & store city
-      var city = weather.city;
-      // Get and store State
-      var state = weather.region;
-      
-      //Get and store thumbnail 
-      var thumb = weather.thumbnail;
-      console.log(thumb);
-      
-      // Output to hooks in HTML
-      $('.temp').text(temp);
-      $('.city').text(city);
-      $('.state').text(state);
-      $('.thumb img').attr('src', thumb)
-      
-      
-      // See console for _weather_ object
-      console.log(weather);
-    },
+$(document).ready(function() {
+  function startTime() {
+      var today = new Date();
+      var h = today.getHours();
+      var m = today.getMinutes();
+      var s = today.getSeconds();
+      var ampm = h >= 12 ? ' PM' : ' AM';
+     
+      hmod= h % 12;
+ 
+    $('#time').text(hmod + ":" + m + ":" + s + ampm);
+      setTimeout(function(){startTime()},500);
+    if( hmod.toString().length === 1) {
+      $('#time').text(hmod + ":" +  m + ":" + s + ampm);
+    };
+    if( m.toString().length === 1) {
+      $('#time').text(hmod + ":" + "0" + m + ":" + s + ampm);
+    };
+    if( s.toString().length === 1) {
+      $('#time').text(hmod + ":" + m + ":" + "0" + s + ampm);
+    };
+  };
+  startTime();
   
-    // if error
-    error: function(error) {  
-      $('body').html('<p>' + error + '</p>');
-    }
+  $("#bluejay").hide();
+  $(".grass").hide();
+  $(".pond1day").hide();
+  $(".pond2day").hide();
+  $(".pond3day").hide();
+  $(".lilypadday").hide();
+  $(".lilypadcutoutday").hide();
   
-  });
+  if (h <= 12) {
+    $(".moon").toggleClass('sun');
+    $(".moon").removeClass('moon');
+    $(".moondot1").removeClass('moondot1');
+    $(".moondot2").removeClass('moondot2');
+    $(".moondot3").removeClass('moondot3');
+    $(".moondot4").removeClass('moondot4');
+    $(".bodynight").toggleClass('bodyday');
+    $(".bodynight").removeClass('bodynight');
+    $(".linebackgroundnight").toggleClass('linebackgroundday');
+    $(".pond1").hide();
+    $(".pond2").hide();
+    $(".pond3").hide();
+    $(".lilypad").hide();
+    $(".lilypadcutout").hide();
+    $(".pond1day").fadeIn();
+    $(".pond2day").fadeIn();
+    $(".pond3day").fadeIn();
+    $(".lilypadday").fadeIn();
+    $(".lilypadcutoutday").fadeIn();
+    $(".owl img").hide();
+    $("#bluejay").fadeIn();
+    $(".grass").fadeIn();
+  };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $.simpleWeather({
+    
+//     location: '99004', // change zip
+//     unit: 'f',
+    
+//     // Get _weather_ object
+//     success: function(weather) {
+      
+//       // Get & store temperature
+//       var temp = weather.temp;
+//       // Get & store city
+//       var city = weather.city;
+//       // Get and store State
+//       var state = weather.region;
+      
+//       //Get and store thumbnail 
+//       var thumb = weather.thumbnail;
+//       console.log(thumb);
+      
+//       // Output to hooks in HTML
+//       $('.temp').text(temp);
+//       $('.city').text(city);
+//       $('.state').text(state);
+//       $('.thumb img').attr('src', thumb)
+      
+      
+//       // See console for _weather_ object
+//       console.log(weather);
+//     },
+  
+//     // if error
+//     error: function(error) {  
+//       $('body').html('<p>' + error + '</p>');
+//     }
+  
+//   });
 
 
 
